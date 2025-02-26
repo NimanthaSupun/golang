cc03b63927e1214aa0624abdb104d4bc2d8d8fba
Nimantha Supun <supunnimantha199@gmail.com>
2025-02-26 20:03:20 +0530

    add basic arithmetic operations in new arithmatic.go file

---
+++ arithmatic/arithmatic.go
@@ -0,0 +1,21 @@
+
+package main;
+
+import "fmt";
+
+func main() {
+
+	a := 10
+	b := 3;
+
+	sum := a + b;
+	fmt.Println(sum);
+
+	var intNum int = 10;
+	var flotNum float64 = 3.5;
+
+	sum1 := intNum + int(flotNum);
+
+	fmt.Println("Sum",sum1);
+
+}
